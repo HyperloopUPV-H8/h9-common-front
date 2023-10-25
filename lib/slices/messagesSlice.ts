fa1f142a5a1cdf6099037de4a8a86457a3f8ad0e
andresdlt <andelatmo@gmail.com>
2023-10-25 17:12:25 +0200

    messagesSlice switched to zustand

--- lib/slices/messagesSlice.ts
+++ lib/slices/messagesSlice.ts
@@ -3,48 +3,54 @@ import { nanoid } from "nanoid";
 import { isEqual } from "lodash";
 import { MessageAdapter } from "../adapters";
 import { Message } from "../models";
+import { StateCreator } from "zustand";
 
-export const messageSlice = createSlice({
-    name: "messages",
-    initialState: [] as Message[],
-    reducers: {
-        addMessage: {
-            reducer(messages: Message[], action: PayloadAction<Message>) {
-                const newMessages = [...messages];
+interface MessagesSlice {
+    messages: Array<Message>
+    addMessage: (message: MessageAdapter) => Array<Message>
+    clearMessages: () => void
+}
 
-                if (
-                    messages.length > 0 &&
-                    areMessagesEqual(
-                        messages[messages.length - 1],
-                        action.payload
-                    )
-                ) {
-                    newMessages[newMessages.length - 1] = {
-                        ...messages[messages.length - 1],
-                        id: action.payload.id,
-                        count: messages[messages.length - 1].count + 1,
-                    };
-                } else {
-                    newMessages.push(action.payload);
-                }
+const messagesSlice: StateCreator<MessagesSlice> = (set, get) => ({
+    messages: [] as Array<Message>,
 
-                return newMessages;
-            },
-            prepare(message: MessageAdapter) {
-                return {
-                    payload: {
-                        id: nanoid(),
-                        count: 1,
-                        ...message,
-                    } as Message,
-                };
-            },
-        },
-        clearMessages: () => {
-            return [];
-        },
+    addMessage: (message: MessageAdapter) => {
+        
+        const preparedMessage = {
+            id: nanoid(),
+            count: 1,
+            ...message
+        } as Message
+
+        if(get().messages.length > 0 && areMessagesEqual(get().messages[get().messages.length - 1], preparedMessage)) {
+            set(state => ({
+                ...state,
+                messages: [
+                    ...state.messages.slice(0, state.messages.length - 1),
+                    {
+                        ...state.messages[state.messages.length - 1],
+                        id: preparedMessage.id,
+                        count: state.messages[state.messages.length - 1].count + 1
+                    }
+                ]
+            }))
+        } else {
+            set(state => ({
+                ...state,
+                messages: [...state.messages, preparedMessage] as Array<Message>
+            }))
+        }
+
+        return [...get().messages, preparedMessage]
     },
-});
+
+    clearMessages: () => {
+        set(state => ({
+            ...state,
+            messages: []
+        }))
+    }
+})
 
 function areMessagesEqual(message: Message, adapter: MessageAdapter): boolean {
     //TODO: this could break easily, i'm not covering the case where they are not the same kind
